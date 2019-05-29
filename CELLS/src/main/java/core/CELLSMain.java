dbc50012dabfc080967aae2083b70c187441473b
Davis <Davis@Arklet>
2019-05-28 21:07:54 -0600

    Commit of the initialization function.  There's also a test to make sure it works correctly.

---
+++ CELLS/src/main/java/core/CELLSMain.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package core;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Davis
+ *
+ */
+public class CELLSMain {
+
+	private List< List< Double > > m_biases = new ArrayList< List< Double > >( );
+	private List< List< List< Double > > > m_weights = new ArrayList< List< List< Double > > >( );
+	
+	public void init( int[ ] p_layers ) {
+		Random rand = new Random( );
+		for( int i = 1; i < p_layers.length; i++ ) {
+			List< Double > layerBias = new ArrayList< Double >( );
+			List< List< Double > > layerWeight = new ArrayList< List< Double > >( );
+			for( int j = 0; j < p_layers[ i ]; j++ ) {
+				layerBias.add( rand.nextGaussian( ) );
+
+				List< Double > connectionWeight = new ArrayList< Double >( );
+				for( int k = 0; k < p_layers[ i - 1 ]; k++ ) {
+					connectionWeight.add( rand.nextGaussian( ) );
+				}
+				layerWeight.add( connectionWeight );
+			}
+			m_biases.add( layerBias );
+			m_weights.add( layerWeight );
+		}
+	}
+
+	/**
+	 * @return the m_biases
+	 */
+	public List< List< Double > > getBiases( ) {
+		return m_biases;
+	}
+
+	/**
+	 * @param p_biases the m_biases to set
+	 */
+	public void setBiases( List< List< Double > > p_biases ) {
+		this.m_biases = p_biases;
+	}
+
+	/**
+	 * @return the m_weights
+	 */
+	public List< List< List< Double > > > getWeights( ) {
+		return m_weights;
+	}
+
+	/**
+	 * @param p_weights the m_weights to set
+	 */
+	public void setWeights( List< List< List< Double > > > p_weights ) {
+		this.m_weights = p_weights;
+	}
+}
---
+++ CELLS/src/main/test/core/CELLSMainTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Davis
+ *
+ */
+class CELLSMainTest {
+
+	/**
+	 * Test method for {@link core.CELLSMain#init(int[])}.
+	 */
+	@Test
+	void testInit( ) {
+		int[ ] testLayers = new int[ ] { 2, 3, 1 };
+		CELLSMain cm = new CELLSMain( );
+		cm.init( testLayers );
+		
+		assertTrue( cm.getBiases( ).size( ) == testLayers.length - 1 );
+		for( int i = 0; i < cm.getBiases( ).size( ); i++ ) {
+			assertTrue( cm.getBiases( ).get( i ).size( ) == testLayers[ i + 1 ] );
+		}
+		
+		assertTrue( cm.getWeights( ).size( ) == testLayers.length - 1 );
+		for( int i = 0; i < cm.getWeights( ).size( ); i++ ) {
+			assertTrue( cm.getWeights( ).get( i ).size( ) == ( testLayers[ i + 1 ] ) );
+			for( int j = 0; j < cm.getWeights( ).get( i ).size( ); j++ ) {
+
+				assertTrue( cm.getWeights( ).get( i ).get( j ).size( ) == ( testLayers[ i ] ) );
+			}
+		}
+	}
+
+}
